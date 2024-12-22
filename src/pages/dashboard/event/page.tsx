775377571e3ee8cc234d3f3c60275047a58ebb94
JiPai <github@jipai.moe>
2024-12-22 15:32:53 +0800

    feat(event): add search function

--- src/pages/dashboard/event/page.tsx
+++ src/pages/dashboard/event/page.tsx
@@ -1,11 +1,11 @@
-import { Button, Group, Title } from "@mantine/core";
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getEventList } from "@/api/dashboard/event";
-import DefaultContainer from "@/components/Container";
-import EventList from "@/pages/dashboard/event/components/EventList";
-import { IconCirclePlus } from "@tabler/icons-react";
-import { useNavigate } from "react-router-dom";
+import { Button, Group, Title } from '@mantine/core';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getEventList } from '@/api/dashboard/event';
+import DefaultContainer from '@/components/Container';
+import EventList from '@/pages/dashboard/event/components/EventList';
+import { IconCirclePlus } from '@tabler/icons-react';
+import { useNavigate } from 'react-router-dom';
 
 export default function EventPage() {
   const navigate = useNavigate();
@@ -13,10 +13,11 @@ export default function EventPage() {
   const [pagination, setPagination] = useState({
     current: 1,
     pageSize: 20,
+    search: undefined,
   });
 
   const { isPending, isError, data, error, refetch } = useQuery({
-    queryKey: ["event-list", pagination],
+    queryKey: ['event-list', pagination],
     queryFn: () => getEventList(pagination),
   });
 
@@ -29,7 +30,7 @@ export default function EventPage() {
           <Button
             leftSection={<IconCirclePlus size={16} stroke={1.5} />}
             onClick={() => {
-              navigate("/dashboard/event/create");
+              navigate('/dashboard/event/create');
             }}
           >
             添加展会
